fcada467bb47b3adda97b87915b2f75453e5a95a
haliim <32435138+haliim@users.noreply.github.com>
2020-11-21 18:34:39 +0200

    Update and rename EC2-Apache to EC2-Apache.sh

---
+++ EC2-Apache.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#testing your EC2 and preinstall apache server
+#Install httpd linux version 2
+yum update -y
+yum install httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hello World I'm Halim from $(hostname -f)" > /var/www/html/index.html
